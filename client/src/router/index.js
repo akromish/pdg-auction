1ca73fa9507d20241dd85e8d4f9490dd9359e17c
akromish <akram.weheba.11@gmail.com>
2021-03-20 00:42:55 -0400

    basic structure in place with vue 2, router, vuetify, and firebase working

---
+++ client/src/router/index.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Admin from '../views/Admin.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/admin',
+    name: 'About',
+    component: Admin
+  },
+
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
---
+++ client/src/views/Admin.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <h1>Howdy Admin Person</h1>
+  </div>
+</template>
+
+<script>
+export default {
+name: "Admin"
+}
+</script>
+
+<style scoped>
+
+</style>
