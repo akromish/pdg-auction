67f878fd6fe924f43b9a347d5173e9d45c62b942
akram <akram.weheba.11@gmail.com>
2021-03-29 13:35:08 -0400

    added bid button from vuetify example

--- src/components/PopUp.vue
+++ src/components/PopUp.vue
@@ -1,13 +1,37 @@
 <template>
-  <v-dialog>
-    <v-btn flat slot="activator" class="success">
-      Bid
-    </v-btn>
-    <v-card>
-      <v-card-title>
-        Complete your bid:
-      </v-card-title>
-    </v-card>
+  <v-dialog transition="dialog-bottom-transition" max-width="600">
+<!--    <v-btn flat slot="activator">-->
+<!--      Bid-->
+<!--    </v-btn>-->
+<!--    <v-card>-->
+<!--      <v-card-title>-->
+<!--        Complete your bid:-->
+<!--      </v-card-title>-->
+<!--    </v-card>-->
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn
+          color="primary"
+          v-bind="attrs"
+          v-on="on"
+      >Bid on Item</v-btn>
+    </template>
+    <template v-slot:default="dialog">
+      <v-card>
+        <v-toolbar
+            color="primary"
+            dark
+        >Opening from the bottom</v-toolbar>
+        <v-card-text>
+          <div class="text-h2 pa-12">Hello world!</div>
+        </v-card-text>
+        <v-card-actions class="justify-end">
+          <v-btn
+              text
+              @click="dialog.value = false"
+          >Close</v-btn>
+        </v-card-actions>
+      </v-card>
+    </template>
   </v-dialog>
 </template>
 
