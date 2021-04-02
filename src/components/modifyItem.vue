b1842a6bcc584be722226d6aec34aba475efb77c
akromish <akram.weheba.11@gmail.com>
2021-04-02 12:09:47 -0400

    more admin stuff

---
+++ src/components/modifyItem.vue
@@ -0,0 +1,121 @@
+<template>
+  <v-dialog transition="dialog-bottom-transition" max-width="600">
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn
+          color="#7b1443"
+          v-bind="attrs"
+          v-on="on"
+          class="white--text"
+      >Edit</v-btn>
+    </template>
+    <template v-slot:default="dialog">
+      <v-card>
+        <v-toolbar
+            color="#143344"
+            dark
+        >{{ itemName }}</v-toolbar>
+        <v-card-actions>
+          <v-form ref="form">
+            <v-text-field
+                label="name"
+                v-model="name"
+                :rules="inputRules"
+                required
+            ></v-text-field>
+            <v-textarea
+                label="description"
+                v-model="description"
+                :rules="inputRules"
+                required
+            ></v-textarea>
+            <v-text-field
+                type="number"
+                label="price"
+                v-model.number="currentPrice"
+                required
+            ></v-text-field>
+            <v-text-field
+                label="bidder"
+                v-model="currentBidder"
+                required
+            ></v-text-field>
+            <v-text-field
+                label="phone number"
+                v-model="phoneNumber"
+                required
+            ></v-text-field>
+          </v-form>
+        </v-card-actions>
+        <v-card-actions>
+          <v-col>
+            <v-btn
+                @click="dialog.value = false"
+            >Cancel</v-btn>
+          </v-col>
+          <v-col class="text-right">
+            <v-btn
+                @click="bid(); dialog.value = false"
+                color="#7b1443"
+            ><div class="white--text">Confirm Edit</div>
+            </v-btn>
+          </v-col>
+        </v-card-actions>
+      </v-card>
+    </template>
+  </v-dialog>
+</template>
+
+<script>
+import { db } from '@/db';
+
+export default {
+  name: "PopUp",
+  methods: {
+    bid () {
+      if(this.$refs.form.validate()) {
+        db.collection("bids").add({
+          name: this.name,
+          bidPrice: this.bidPrice,
+          phoneNumber: this.phoneNumber,
+          itemName: this.itemName
+        })
+        db.collection("items").doc(this.itemName).update({
+          currentPrice: this.bidPrice,
+          currentBidder: this.name,
+          phoneNumber: this.phoneNumber,
+        })
+        this.$emit('changeStuff', 'hmmm');
+      }
+    },
+  },
+  props: {
+    itemName: String,
+    currentPrice: Number,
+    currentBidder: String,
+  },
+  data() {
+    return {
+      name: '',
+      phoneNumber: '',
+      bidMin: this.currentPrice + 1,
+      bidPrice: this.bidMin,
+      inputRules: [
+        v => v.length >=3 || 'please enter your full name',
+      ],
+      bidRules: [
+        v => v >= this.bidMin || `minimum bid: $${this.bidMin}`,
+      ],
+      phoneRules: [
+        v => v.length >=9 || 'please enter your phone number',
+      ],
+      valid: true,
+    }
+  },
+}
+</script>
+
+<style scoped>
+.v-toolbar {
+  font-size: 1.5rem;
+}
+</style>
--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -3,20 +3,53 @@
     <div class="text-center my-4">
       <v-btn @click="add">Add Item</v-btn>
     </div>
-    <v-data-table
-        class="mx-4"
-        :headers="headers"
-        :items="items"
-        :items-per-page="10"
-    ></v-data-table>
+    <v-container class="my-5">
+      <v-layout row wrap>
+        <v-flex xs12 sm6 md4 lg3 v-for="item in items" :key="item.name">
+          <v-card text class="my-3 mx-4 justify-center rounded-md" color="#fffff2">
+            <v-card-title>
+              <div>{{ item.name }}</div>
+            </v-card-title>
+            <v-card-text>
+              <v-col>
+                <div>Bidder: {{ item.currentBidder }}</div>
+                <div>Phone Number: {{ item.phoneNumber }}</div>
+                <div class="font-weight-bold">Current Price: ${{ item.currentPrice }}</div>
+
+              </v-col>
+            </v-card-text>
+            <v-card-actions>
+              <v-col class="text-left">
+                <v-btn color="#FF0000" class="white--text">Delete</v-btn>
+              </v-col>
+              <v-col class="text-right">
+                <modifyItem
+                    v-bind:item-name="item.name"
+                    v-bind:current-bidder="item.currentBidder"
+                    v-bind:current-price="item.currentPrice"
+                    v-on:changeStuff="updateStuff($event)"
+                />
+              </v-col>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+
+
   </div>
 </template>
 
 <script>
 import {db} from "@/db";
+import modifyItem from "@/components/modifyItem";
+
 export default {
   name: "Admin",
   title: "Shumail's Page",
+  components: {
+    modifyItem,
+  },
   methods: {
     add() {
       this.$router.replace({name: 'Add Item'});
